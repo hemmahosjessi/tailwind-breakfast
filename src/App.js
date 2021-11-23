bba428047fe0ed1bae6601a2ef3ebc0e745f3917
Jessica Nygren <jessicanygren@C02D54BJMD6R.local>
2021-11-23 10:33:43 +0100

    breakfast code along that didn realy work with updating real time

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const App = () => {
   return (
-    <div>
-      <h1>Hello!</h1>
+    <div className='flex justify-between my-4'>
+      <h1 className='text-green-50'>Hello!</h1>
       <h1>World!</h1>
     </div>
   )
